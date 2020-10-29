297457fd88a7c27fe92ec84c9104012539c303da
cristian samir quejada cordoba <cristiansamir2001@gmail.com>
2020-10-29 19:32:08 +0100

    se agrega tipografia de terminator para el titulo

---
+++ HOLA MUNDO/index (2).php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+
+          <head>
+                    <title>
+                    MI PRIMERA PAGINA
+                    </title>
+                    <meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="estilos.css">
+                
+          </head>
+          <body>
+
+           
+           <h1>Primera página Anacleto</h1>
+
+          </body>
+
+</html>
